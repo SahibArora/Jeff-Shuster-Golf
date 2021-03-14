0c8631f538d1acdf29ccc1b44a64a87bbb29a28c
Sahib Arora <sahibarora1997@gmail.com>
2021-03-14 08:19:40 -0400

    Contact Form Front-end updated

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,3 +1,15 @@
+window.onload = (event) => {
+    if(document.getElementById('serviceInterest').value == ''){
+        document.getElementById('time_slots').style.display = 'none'
+        document.getElementById('date').required = false
+        document.getElementById('timeSlot').required = false
+    }else{
+        document.getElementById('time_slots').style.display = ''
+        document.getElementById('date').required = true
+        document.getElementById('timeSlot').required = true
+    }
+}
+
 function validateEmail(){
     var email = document.getElementById('email-form').value;
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -10,3 +22,15 @@ function validateEmail(){
     document.getElementById('email-form').style.borderColor = 'green'
     return true
 }   
+
+function showTimeSlots() {
+    if(document.getElementById('serviceInterest').value == ''){
+        document.getElementById('time_slots').style.display = 'none'
+        document.getElementById('date').required = false
+        document.getElementById('timeSlot').required = false
+    }else{
+        document.getElementById('time_slots').style.display = ''
+        document.getElementById('date').required = true
+        document.getElementById('timeSlot').required = true
+    }
+}
