8168e0a6ade60375b2e92ee7c3114b20d12bb715
Sahib Arora <sahibarora1997@gmail.com>
2021-03-26 14:07:09 -0400

    Store header

--- public/js/contact.js
+++ public/js/contact.js
@@ -14,11 +14,11 @@ function validateEmail(){
     var email = document.getElementById('email-form').value;
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(re.test(String(email).toLowerCase()) == false){
-        alert('Please enter Valid email address');
+        document.getElementById('errorEmail').style.display = ''
         document.getElementById('email-form').style.borderColor = 'red'
         return false
     }
-
+    document.getElementById('errorEmail').style.display = 'none'
     document.getElementById('email-form').style.borderColor = 'green'
     return true
 }   
---
+++ public/js/shop.js
@@ -0,0 +1,5 @@
+function changeCart(){
+    document.getElementById('cart').src = 'images/cart_filled.png'
+    document.getElementById('cart').style.height = '35px'
+    document.getElementById('cart').style.width = '40px'
+}
